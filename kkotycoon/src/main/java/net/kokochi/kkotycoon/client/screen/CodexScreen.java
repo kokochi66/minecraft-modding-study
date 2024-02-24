525d66461975fc24b0e9671fdd44210943da8947
kokochi66 <61536109+kokochi66@users.noreply.github.com>
2024-02-24 21:17:13 +0900

    gui 수정

--- kkotycoon/src/main/java/net/kokochi/kkotycoon/client/screen/CodexScreen.java
+++ kkotycoon/src/main/java/net/kokochi/kkotycoon/client/screen/CodexScreen.java
@@ -2,31 +2,73 @@ package net.kokochi.kkotycoon.client.screen;
 
 import net.kokochi.kkotycoon.client.data.CodexSet;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.FontManager;
+import net.minecraft.client.font.FontStorage;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.render.OutlineVertexConsumerProvider;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.BakedModel;
 import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.texture.Sprite;
+import net.minecraft.client.texture.StatusEffectSpriteManager;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import org.joml.Matrix4f;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 // 도감 화면 GUI를 개발
 public class CodexScreen extends Screen {
+    private List<Rectangle> itemSlotPositions;
+    private List<ItemStack> itemStacks;
+    private List<Text> itemTooltips;
+    private Rectangle[] arrowPositions;
 
     private final int MAX_PAGE = 2;
     private int currentPage = 0;
     public CodexScreen(Text title) {
         super(title);
     }
+
+    private void initPositionData() {
+        // 도감 아이템 슬롯
+        itemSlotPositions = new ArrayList<>();
+        int slotBoxSize = 20;
+        int padding = 2;
+        int yOffset = 10; // 제목 영역 고려
+        for (int j = 0; j < 7; j++) {
+            for (int i = 0; i < 15; i++) {
+                int x = this.width / 2 - (15 * (slotBoxSize + padding) / 2) + (i * (slotBoxSize + padding));
+                int y = this.height / 2 - (7 * (slotBoxSize + padding) / 2) + (j * (slotBoxSize + padding)) + yOffset;
+                itemSlotPositions.add(new Rectangle(x, y, slotBoxSize, slotBoxSize));
+            }
+        }
+        itemStacks = Arrays.stream(CodexSet.values()).map(c -> new ItemStack(c.getItem())).collect(Collectors.toList());
+        itemTooltips = itemStacks.stream().map(itemStack -> getTooltipFromItem(Objects.requireNonNull(this.client), itemStack).stream().findFirst().orElse(Text.literal("none"))).collect(Collectors.toList());
+
+        int leftArrowX = 10; // 화살표 위치 조정 필요
+        int rightArrowX = this.width - 30; // 화살표 위치 조정 필요
+        int arrowSize = 20;
+        int arrowY = this.height / 2 - 10; // 화살표 위치 조정 필요
+        // 도감 왼쪽, 오른쪽 화살표
+        arrowPositions = new Rectangle[2];
+        arrowPositions[0] = new Rectangle(leftArrowX, arrowY, arrowSize, arrowSize);
+        arrowPositions[1] = new Rectangle(rightArrowX, arrowY, arrowSize, arrowSize);
+    }
+
+
 
     // 페이지 변경 메서드
     private void changePage(int direction) {
@@ -43,6 +85,7 @@ public class CodexScreen extends Screen {
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         renderBackground(context); // 기본 배경 렌더링
+        initPositionData();        // 포지션 관련 정보를 렌더링 할 때마다 초기화 (그때그때마다 화면 비율을 갖고 계산해야하기 때문)
 
         super.render(context, mouseX, mouseY, delta);
 
@@ -50,77 +93,67 @@ public class CodexScreen extends Screen {
         context.fill(0, 0, this.width, this.height, 0x01000000); // 반투명 검은색
 
         // 화살표 버튼 그리기
-        int leftArrowX = 10; // 화살표 위치 조정 필요
-        int rightArrowX = this.width - 30; // 화살표 위치 조정 필요
-        int arrowSize = 20;
-        int arrowY = this.height / 2 - 10; // 화살표 위치 조정 필요
-        context.fill(leftArrowX, arrowY, leftArrowX + arrowSize, arrowY + arrowSize, 0xFF0000FF); // 왼쪽 화살표 (색상 변경 필요)
-        context.fill(rightArrowX, arrowY, rightArrowX + arrowSize, arrowY + arrowSize, 0xFF0000FF); // 오른쪽 화살표 (색상 변경 필요)
-
+        context.fill(arrowPositions[0].x, arrowPositions[0].y, arrowPositions[0].x + arrowPositions[0].width, arrowPositions[0].y + arrowPositions[0].height, 0xFF0000FF); // 왼쪽 화살표 (색상 변경 필요)
+        context.fill(arrowPositions[1].x, arrowPositions[1].y, arrowPositions[1].x + arrowPositions[1].width, arrowPositions[1].y + arrowPositions[1].height, 0xFF0000FF); // 왼쪽 화살표 (색상 변경 필요)
 
         // 도감 달성도 표시
-        TextRenderer textRenderer = MinecraftClient.getInstance().textRenderer;
         int achievedCount = 0; // 달성한 도감 수 (변수로 처리)
-        String achievementText = "도감 달성도 " + achievedCount + "/300";
-        int textWidth = textRenderer.getWidth(achievementText); // 텍스트 너비 계산
-        int textX = this.width / 2 - textWidth / 2; // 텍스트 X 좌표 (가운데 정렬)
-        int textY = 30; // 텍스트 Y 좌표 (상단에서부터의 거리)
-        context.drawText(textRenderer, achievementText, textX, textY, 0xFFFFFFFF, true); // 텍스트 그리기
+        String titleText = "도감 달성도 " + achievedCount + "/300";
+        int titleWidth = textRenderer.getWidth(titleText); // 텍스트 너비 계산
+        int titleX = this.width / 2 - titleWidth / 2; // 텍스트 X 좌표 (가운데 정렬)
+        int titleY = 30; // 텍스트 Y 좌표 (상단에서부터의 거리)
+        context.drawText(textRenderer, titleText, titleX, titleY, 0xFFFFFFFF, true); // 텍스트 그리기
 
+        Identifier identifier = new Identifier("textures/block/dirt.png");
         // 아이템 슬롯 그리기
-        // 테스트용 흙블럭 텍스쳐를 띄워줌
         // 아이템 렌더링 준비
         List<ItemStack> itemStacks = Arrays.stream(CodexSet.values()).map(c -> new ItemStack(c.getItem())).toList();
-        int slotBoxSize = 20;
-        int padding = 2;
-        int yOffset = 10; // 아래로 이동시키고 싶은 픽셀 값 (제목 영역 고려)
+        int iconOffset = 2;
+        for (int i = 0; i < 105; i ++) {
+            Rectangle rectangle = itemSlotPositions.get(i);
+            int x = rectangle.x;
+            int y = rectangle.y;
+            int slotBoxSize = rectangle.width;
 
-        int iconSize = 16;
-        int iconOffset = (slotBoxSize - iconSize) / 2;
-        for (int j = 0; j < 7; j ++) {
-            for (int i = 0; i < 15; i++) {
-                int x = this.width / 2 - (15 * (slotBoxSize + padding) / 2) + (i * (slotBoxSize + padding)); // x 위치 계산
-                int y = this.height / 2 - (7 * (slotBoxSize + padding) / 2) + (j * (slotBoxSize + padding)) + yOffset; // y 위치 계산
-                context.fill(x, y, x + slotBoxSize, y + slotBoxSize, 0x88555555); // 더 큰 어두운 사각형
+            context.fill(x, y, x + slotBoxSize, y + slotBoxSize, 0x88555555); // 더 큰 어두운 사각형
 
-                int stackIndex = (currentPage * 100) + (j * 15) + i;
-                if (stackIndex >= itemStacks.size()) {
-                    stackIndex = itemStacks.size() - 1;
-                }
-                ItemStack itemStack = itemStacks.get(stackIndex);
-                // 마우스가 아이템 슬롯 위에 있는지 확인
-                if (mouseX >= x && mouseX <= x + slotBoxSize && mouseY >= y && mouseY <= y + slotBoxSize) {
-                    // 마우스 위치에 따라 아이템 이름 표시
-                    List<Text> tooltip = getTooltipFromItem(MinecraftClient.getInstance(), itemStack);
-                    // 툴팁 설정
-                    this.setTooltip(tooltip.stream().findAny().orElse(Text.literal("none")));
-                }
-
-                // 흙 블럭 렌더링
-                context.drawItem(itemStack, x + iconOffset, y + iconOffset);
+            int stackIndex = (currentPage * 105) + i;
+            if (stackIndex >= itemStacks.size()) {
+                stackIndex = itemStacks.size() - 1;
             }
-        }
+            ItemStack itemStack = itemStacks.get(stackIndex);
+            // 마우스가 아이템 슬롯 위에 있는지 확인
+            if (mouseX >= x && mouseX <= x + slotBoxSize && mouseY >= y && mouseY <= y + slotBoxSize) {
+                this.setTooltip(itemTooltips.get(stackIndex));
+            }
 
+            // 아이템 이미지 렌더링
+//            context.drawItem(itemStack, x + iconOffset, y + iconOffset);
+            context.drawTexture(identifier, x + iconOffset, y + iconOffset, 16, 16, 0, 0, 16, 16, 16, 16);
+//            // 도감 달성을 위해 필요한 개수 지정 (일단 디폴드로 10개로 설정)
+            context.drawText(textRenderer, "10", x + 8, y + 10, 0xFFFFFFFF, true); // 텍스트 그리기
+//            context.drawTooltip(textRenderer, Text.of("10"), x + 8, y + 10);
+        }
     }
 
     // 화살표 클릭 이벤트 처리
     @Override
     public boolean mouseClicked(double mouseX, double mouseY, int button) {
-        // 화살표 버튼 영역 계산 필요
-        int leftArrowX = 10; // 화살표 위치 조정 필요
-        int rightArrowX = this.width - 30; // 화살표 위치 조정 필요
-        int arrowSize = 20;
-        int arrowY = this.height / 2 - 10; // 화살표 위치 조정 필요
-        if (mouseX >= leftArrowX && mouseX <= leftArrowX + arrowSize && mouseY >= arrowY && mouseY <= arrowY + arrowSize) {
-            changePage(-1); // 이전 페이지로
-            return true;
-        } else if (mouseX >= rightArrowX && mouseX <= rightArrowX + arrowSize && mouseY >= arrowY && mouseY <= arrowY + arrowSize) {
-            changePage(1); // 다음 페이지로
-            return true;
+
+        if (button == 0) {
+            if (mouseX >= arrowPositions[0].x && mouseX <= arrowPositions[0].x + arrowPositions[0].width && mouseY >= arrowPositions[0].y && mouseY <= arrowPositions[0].y + arrowPositions[0].height) {
+                changePage(-1); // 이전 페이지로
+                return true;
+            } else if (mouseX >= arrowPositions[1].x && mouseX <= arrowPositions[1].x + arrowPositions[1].width && mouseY >= arrowPositions[1].y && mouseY <= arrowPositions[1].y + arrowPositions[1].height) {
+                changePage(1); // 다음 페이지로
+                return true;
+            }
         }
+
+
+
         return super.mouseClicked(mouseX, mouseY, button);
     }
-
 
 
     // esc를 눌러서 닫을 수 있는지 여부 설정
